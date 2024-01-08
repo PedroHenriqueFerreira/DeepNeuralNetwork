b565990769c305043fa6f1af48c4e44a5ea56378
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2024-01-08 13:25:22 -0300

    Generator UI

--- test.py
+++ test.py
@@ -6,39 +6,43 @@ import numpy as np
 
 from main import GAN
 
-gan = pkl.load(open('./GAN.pkl', 'rb'))
+with open('./GAN.pkl', 'rb') as file:
+    gan = pkl.load(file)
 
 root = Tk()
 
-canvas = Canvas(root, width=512, height=512)
+CANVAS_SIZE = 128
+
+canvas = Canvas(root, width=CANVAS_SIZE, height=CANVAS_SIZE)
 
 def generate():
     noise = np.random.randn(1, 100)
     
     image = gan.generator.forward(noise, False)[0]
     image = (image * 127.5 + 127.5).astype(np.uint8)
+    image = image.transpose((1, 2, 0))
     
-    size = 512 // 128
+    size = CANVAS_SIZE // image.shape[0]
     
-    for i in range(128):
-        for j in range(128):
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
             i0 = i * size
             j0 = j * size
+            i1 = i0 + size
+            j1 = j0 + size
             
-            i1 = (i + 1) * size
-            j1 = (j + 1) * size
-            
-            r = f'{image[0, i, j]:02x}'
-            g = f'{image[1, i, j]:02x}'
-            b = f'{image[2, i, j]:02x}'
+            r = f'{image[j][i][0]:02x}'
+            g = f'{image[j][i][1]:02x}'
+            b = f'{image[j][i][2]:02x}'
             
             rgb = f'#{r}{g}{b}'
             
             canvas.create_rectangle(i0, j0, i1, j1, fill=rgb, width=0)
 
+
 button = Button(root, text='Generate', command=generate)
 
-canvas.pack()
-button.pack(expand=True, fill='x')
+canvas.pack(expand=True)
+button.pack(expand=True)
 
 root.mainloop()
